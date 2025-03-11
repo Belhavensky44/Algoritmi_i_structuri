a0f077fe9e8c0813b632c73371c082af0c179cc4
Belhavensky <Schuleeee123@yandex.ru>
2025-03-11 15:55:29 +0500

    Внесение изменений согласно правкам

---
+++ Семестр 2/Лабараторная 1/IHighInterfaceArray.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace OOP_
+{
+    public interface IHighInterfaceArray
+    {
+        bool Find(long searchValue);
+        void Insert(long value);
+        bool Delete(long value);
+        void Display();
+        int GetSize();
+        long FindMin();
+        long FindMax();
+    }
+}
