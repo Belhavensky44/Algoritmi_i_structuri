afb0f95a6f954d0ea3aaf54a478122d15f3027d4
Belhavensky44 <Schuleeee123@yandex.ru>
2025-03-23 23:29:39 +0500

    Create IHighInterfaceArray.cs

---
+++ Семестр 2/Лабораторная 3/IHighInterfaceArray.cs
@@ -0,0 +1,9 @@
+public interface IHighInterfaceArray
+{
+    void Insert(long value);
+    void Display();
+    int GetSize();
+    void SortShell(); 
+    void SortQuick(); 
+    void SortMerge(); 
+    HighInterfaceArrayImpl Clone();
